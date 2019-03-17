1dc755ad54848ee22d8d1f7256ab856d95737444
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2019-03-16 23:57:29 -0400

    fix: correct where plugin gets installed to

--- tests/setup.sh
+++ tests/setup.sh
@@ -9,10 +9,9 @@ fi
 echo "Dokku version $DOKKU_VERSION"
 
 set -x
-export DOKKU_ROOT="~dokku"
+export DOKKU_LIB_ROOT="/var/lib/dokku"
 source "$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")/config"
-rm -rf "$DOKKU_ROOT/plugins/$PLUGIN_COMMAND_PREFIX"
-mkdir -p "$DOKKU_ROOT/plugins/$PLUGIN_COMMAND_PREFIX" "$DOKKU_ROOT/plugins/$PLUGIN_COMMAND_PREFIX/subcommands"
-find ./ -maxdepth 1 -type f -exec cp '{}' "$DOKKU_ROOT/plugins/$PLUGIN_COMMAND_PREFIX" \;
-find ./subcommands -maxdepth 1 -type f -exec cp '{}' "$DOKKU_ROOT/plugins/$PLUGIN_COMMAND_PREFIX/subcommands" \;
-echo "$DOKKU_VERSION" > $DOKKU_ROOT/VERSION
+rm -rf "$DOKKU_LIB_ROOT/plugins/$PLUGIN_COMMAND_PREFIX"
+mkdir -p "$DOKKU_LIB_ROOT/plugins/$PLUGIN_COMMAND_PREFIX" "$DOKKU_LIB_ROOT/plugins/$PLUGIN_COMMAND_PREFIX/subcommands"
+find ./ -maxdepth 1 -type f -exec cp '{}' "$DOKKU_LIB_ROOT/plugins/$PLUGIN_COMMAND_PREFIX" \;
+find ./subcommands -maxdepth 1 -type f -exec cp '{}' "$DOKKU_LIB_ROOT/plugins/$PLUGIN_COMMAND_PREFIX/subcommands" \;
