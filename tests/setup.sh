7f201847f0ee05e7e64dafbcc840892a3ca7caad
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2019-03-17 00:37:23 -0400

    fix: add missing env var for config source

--- tests/setup.sh
+++ tests/setup.sh
@@ -10,11 +10,12 @@ echo "Dokku version $DOKKU_VERSION"
 
 set -x
 export DOKKU_PLUGINS_ROOT="/var/lib/dokku/plugins/available"
+export DOKKU_LIB_ROOT="/var/lib/dokku"
 source "$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")/config"
 sudo rm -rf "$DOKKU_PLUGINS_ROOT/$PLUGIN_COMMAND_PREFIX"
 sudo mkdir -p "$DOKKU_PLUGINS_ROOT/$PLUGIN_COMMAND_PREFIX" "$DOKKU_PLUGINS_ROOT/$PLUGIN_COMMAND_PREFIX/subcommands"
 sudo find ./ -maxdepth 1 -type f -exec cp '{}' "$DOKKU_PLUGINS_ROOT/$PLUGIN_COMMAND_PREFIX" \;
 sudo find ./subcommands -maxdepth 1 -type f -exec cp '{}' "$DOKKU_PLUGINS_ROOT/$PLUGIN_COMMAND_PREFIX/subcommands" \;
-sudo mkdir "$PLUGIN_CONFIG_ROOT" "$PLUGIN_DATA_ROOT"
+sudo mkdir -p "$PLUGIN_CONFIG_ROOT" "$PLUGIN_DATA_ROOT"
 sudo dokku plugin:enable "$PLUGIN_COMMAND_PREFIX"
 sudo dokku plugin:install
